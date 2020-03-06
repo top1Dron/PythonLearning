280d230e78facf444f5c7fa549505c6033dfcdcf
top1Dron <pro100takvoin@gmail.com>
2020-03-06 21:47:17 +0200

    creating and using my modules

--- PythonLearning/base_syntax/from_import.py
+++ PythonLearning/base_syntax/from_import.py
@@ -1,20 +1,21 @@
 from math import *
-n = input('Enter number: ')
+
+n = int(input('Enter number: '))
 p = [2, 3]
 cnt = 2
 a = 5
-while (cnt < n):
+while cnt < n:
     b = 0
-    for i in range (2, a):
-        if( i <= sqrt(a)):
-            if(a % i == 0):
+    for i in range(2, a):
+        if i <= sqrt(a):
+            if a % i == 0:
                 print("a neprost", a)
                 b = 1
             else:
                 pass
-    if(b != 1):
+    if b != 1:
         print("a prost", a)
         p += [a]
-    cnt+=1
-    a+=2
+    cnt += 1
+    a += 2
 print(p)
---
+++ PythonLearning/base_syntax/using_name.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    print('itself')
+else:
+    print('imported from another module')
