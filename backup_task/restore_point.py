c2bc0bd73f7e8ca0981f1185a4ccfb08f6267b67
top1Dron <pro100takvoin@gmail.com>
2020-03-07 20:04:44 +0200

    restore_point v2 (creating restore point using pyriodic)

--- backup_task/restore_point.py
+++ backup_task/restore_point.py
@@ -1,14 +1,20 @@
 import os
-import time
+import pyriodic
 
 
-def docFunction():
+def createRestorePoint():
     """This program creates restore point for Windows
 
     """
+    __version__ = 1.0
+    print(createRestorePoint.__doc__)
+    default = 'WMIC /Namespace:\\\\root\\default Path SystemRestore Call CreateRestorePoint "RestorePoint", 100, 7'
+    os.system(default)
 
 
-__version__ = 1.0
-print(docFunction.__doc__)
-default = 'WMIC /Namespace:\\\\root\\default Path SystemRestore Call CreateRestorePoint "RestorePoint", 100, 7'
-os.system(default)
+task = pyriodic.Scheduler()
+task.add_job(pyriodic.DatetimesJob(
+    createRestorePoint,
+    when='7:40 pm',
+    interval='daily'
+))
