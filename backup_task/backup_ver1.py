5af8f1c28b5b80d8fae2ad5267f94359ef5dea1c
top1Dron <pro100takvoin@gmail.com>
2020-03-07 14:24:23 +0200

    backup_task v.1

---
+++ backup_task/backup_ver1.py
@@ -0,0 +1,19 @@
+import os
+import time
+
+# files and packages will be in list
+source = ['"D:\\мемы"']
+
+# backup dir
+targetDir = '"D:\\BackUpPython"'
+
+# all files parse to zip with date name
+target = targetDir + os.sep + time.strftime('%Y%m%d%H%M%S') + '.zip'
+
+zip_command = "zip -qr {} {}".format(target, ' '.join(source))
+
+# start creating back up
+if os.system(zip_command) == 0:
+    print('Backup zip file created in directory:', target)
+else:
+    print('Backup failed.')
