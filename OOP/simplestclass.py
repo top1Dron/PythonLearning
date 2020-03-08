5313f7569994ef185ae00192ae912e518ebb4873
top1Dron <pro100takvoin@gmail.com>
2020-03-08 14:55:58 +0200

    declared constructor for class Person with 1 parameter

--- OOP/simplestclass.py
+++ OOP/simplestclass.py
@@ -1,7 +1,12 @@
 class Person:
+    def __init__(self, name):
+        self.name = name
+
     def sayHi(self):
-        print("Hi, how it's going on?")
+        print("Hi, I am {}. How it's going on?".format(self.name))
 
 
-p = Person()
+p = Person('Dron')
 p.sayHi()
+
+# Person('Dron').sayHi()
